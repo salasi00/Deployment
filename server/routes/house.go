e2bd672ca2dfb5d0bbafd077af8de7c61f48fa89
salasi00 <bogeymanyolo@gmail.com>
2023-02-14 05:18:32 +0700

    deploy

--- server/routes/house.go
+++ server/routes/house.go
@@ -16,7 +16,7 @@ func HouseRoutes(r *mux.Router) {
 	r.HandleFunc("/houses", h.FindHouse).Methods("GET")
 	r.HandleFunc("/house/{id}", h.GetHouse).Methods("GET")
 	// r.HandleFunc("/house", h.CreateHouse).Methods("POST")
-	r.HandleFunc("/house", middleware.Auth(middleware.UploadFile(h.CreateHouse))).Methods("POST")
+	r.HandleFunc("/house", middleware.UploadFile(h.CreateHouse)).Methods("POST")
 	r.HandleFunc("/house/{id}", h.UpdateHouse).Methods("PATCH")
 	r.HandleFunc("/house/{id}", h.DeleteHouse).Methods("DELETE")
 	// r.HandleFunc("/house", middlewre.Autha(h.CreateHouse)).Methods("POST")
